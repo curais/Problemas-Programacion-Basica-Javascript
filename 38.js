d10e994d94c6c2393a7758a9c92766a3bab7d3b9
Cris <craisochoa22@gmail.com>
2019-09-19 21:13:34 -0600

    Resueltos mas ejercicios

--- 38.js
+++ 38.js
@@ -2,3 +2,18 @@
 38. Ordenar una matriz de M filas y N columnas por la primera columna utilizando
     SHELL (por insercion)
 */
+function genMatrix(m, n){
+    let matrix =  [];
+    for(let i = 0; i < m; i++){
+        matrix.push([]);
+        for(let j = 0; j < n; j++){
+            matrix[i][j] = Math.random();
+        }
+    }
+    return matrix;
+}
+
+console.log(genMatrix(99,1).sort((a,b) => {
+    if(a[0] === b[0]) return 0;
+    return (a[0] < b[0]) ? -1 : 1;
+}));
--- 39.js
+++ 39.js
@@ -2,3 +2,16 @@
 39. Crear una tabla de 3 paginas, 4 filas y 5 columnas done el primer elemnto valga1 1
     el segundo 2 y asi sucesivamente
 */
+let count = 0;
+let str = ""
+for(let pagina = 0; pagina < 3; pagina++){
+    str += `Tabla #${pagina+1}\n`;
+    for(let fila = 0; fila < 4; fila++){
+        for(let columna = 0; columna < 5; columna++){
+            str += `${++count}\t`;
+        }
+        str += '\n';
+    }
+}
+
+console.log(str);
